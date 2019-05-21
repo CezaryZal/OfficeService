09d284e4709b9c7fbdaf07ab7428679855906e2d
Cezary Zalewski <czarek0821@gmail.com>
2019-05-21 21:08:06 +0200

    add start program command

--- src/OfficeService.java
+++ src/OfficeService.java
@@ -1,5 +1,10 @@
-public class OfficeService {
-    public static void main (String [] args){
+import java.text.ParseException;
 
+public class OfficeService {
+    public static void main(String[] args) throws ParseException {
+
+        Menu menu = new Menu();
+        menu.startMenu();
+        
     }
 }
