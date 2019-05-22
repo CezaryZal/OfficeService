da6abebc9efd567ea5f62c1d1cab572e23c06bdf
Cezary Zalewski <czarek0821@gmail.com>
2019-05-22 21:20:30 +0200

    create Screen class

---
+++ src/Screen.java
@@ -0,0 +1,10 @@
+public class Screen {
+
+    public void printText (String text){
+        System.out.println(text);
+    }
+
+    public void printText (Object object){
+        System.out.println(object);
+    }
+}
