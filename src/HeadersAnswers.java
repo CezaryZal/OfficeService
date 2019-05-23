38fc68114de10e18c8f881e62197d3822ef514df
Cezary Zalewski <czarek0821@gmail.com>
2019-05-23 21:56:13 +0200

    create `HeadersAnswers` class`

---
+++ src/HeadersAnswers.java
@@ -0,0 +1,15 @@
+public interface HeadersAnswers {
+
+    String headerToListEmployments = "Lista wszystkich dostępnych pracowników: \n";
+
+    String headerToListEmploymentsShortData = "Lista wszystkich dostępnych pracowników wraz z ich podstawowymi danymi.\n";
+
+    String headerToListEmploymentsShortDataAndDateEmployment =
+            "Lista wszystkich dostępnych pracowników. Numer id, imię nazwisko oraz date zatrudnienia.\n";
+
+    String headerToListEmploymentsShortDataAndSex =
+            "Lista wszystkich dostępnych pracowników. Numer id, imię nazwisko oraz płeć.\n";
+
+    String headerToListEmploymentsShortDataAndNumberDepartment =
+            "Lista wszystkich dostępnych pracowników. Numer id, imię nazwisko oraz numere identyfikacyjny dział.\n";
+}
