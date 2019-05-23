4a9e971f05ac29a4e87021d68b69c69f3e2df24b
Cezary Zalewski <czarek0821@gmail.com>
2019-05-23 21:57:37 +0200

    create `HeadersQuestion` class`

---
+++ src/HeadersQuestions.java
@@ -0,0 +1,25 @@
+public interface HeadersQuestions {
+
+    String mainManu = "\nProszę wpisać numer w celu przejścia do odpowieniej funkcji menu:  \n" +
+            "1 - Wyświetlenie najważniejszych informacji o wszystkich pracownikach\n" +
+            "2 - Wyświetlenie szczegółowych informacji\n" +
+            "3 - Możliwość dodania nowych pracowników\n" +
+            "4 - Możliwość usunięcia pracownika\n" +
+            "5 - Możliwośc edytowania danych poszczególnego pracownika\n" +
+            "6 - Dodatkowe funkcje\n" +
+            "Aby zakończyć pracę programu wpisać \"stop Program\"";
+
+    String optionForSelectDetailsData = "Szczegółowe infomracje dotyczyć mają: \n" +
+            "1 - Wszystkich pracowników \n" +
+            "2 - Konkretnego pracownika \n" +
+            "W celu powrotu do menu głównego, proszę wpisać \"back\"";
+
+    String optionForSelectCategoryData = "Wpisanie odpowiedniej liczby wyświetli interesującą informację wszystkich pracowników: \n" +
+            "1 - datę zatrudnienia \n" +
+            "2 - płeć \n" +
+            "3 - numer identyfikacyjny działu ";
+
+    String optionForSelectEmployer = "Którego pracownika z pośród poniższej listy mają wyświetlić się szczegółowe informacje? Proszę wpisać jego id: ";
+
+    String farewellText = "Program został zakończony";
+}
