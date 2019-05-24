bed33627b3dfc4659336d47578a0da1a4a07fbf5
Cezary Zalewski <czarek0821@gmail.com>
2019-05-24 19:13:37 +0200

    add next questions

--- src/HeadersQuestions.java
+++ src/HeadersQuestions.java
@@ -3,7 +3,7 @@ public interface HeadersQuestions {
     String mainManu = "\nProszę wpisać numer w celu przejścia do odpowieniej funkcji menu:  \n" +
             "1 - Wyświetlenie najważniejszych informacji o wszystkich pracownikach\n" +
             "2 - Wyświetlenie szczegółowych informacji\n" +
-            "3 - Możliwość dodania nowych pracowników\n" +
+            "3 - Możliwość dodania nowego pracownika\n" +
             "4 - Możliwość usunięcia pracownika\n" +
             "5 - Możliwośc edytowania danych poszczególnego pracownika\n" +
             "6 - Dodatkowe funkcje\n" +
@@ -19,7 +19,18 @@ public interface HeadersQuestions {
             "2 - płeć \n" +
             "3 - numer identyfikacyjny działu ";
 
-    String optionForSelectEmployer = "Którego pracownika z pośród poniższej listy mają wyświetlić się szczegółowe informacje? Proszę wpisać jego id: ";
+    String optionForSelectEmployer = "Którego pracownika z pośród powyższej listy mają wyświetlić się szczegółowe informacje? Proszę wpisać jego id: ";
+
+    String headerToCreateNewEmployer = "W celu dodania nowego pracownika proszę wpisać o nim dane w odpowiedniej kolejności lub " +
+            "\"back\", aby wrócić do głównego menu.\n" +
+            "('last name', 'first name', 'date employment', 'sex', 'number department', 'name department', 'position', " +
+            "'number phone', 'email', 'supervisor', 'hobby') \n" +
+            "Przykład: Zawadzki, Damian, 2014-06-02, M, 3, HR, junior HR, 968145252, damian@wp.pl, Krzysztof Dabrowski, TV movie\n";
+
+    String headerToDeleteEmployer = "Proszę wpisać id pracownika, który ma zostać usunięty z pośród listy lub " +
+            "\"back\" aby wrócić do głównego menu: \n";
+
+
 
     String farewellText = "Program został zakończony";
 }
