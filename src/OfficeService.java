57c262b20b2421a88508271936debcc7a0b5450e
Cezary Zalewski <czarek0821@gmail.com>
2019-05-29 22:21:44 +0200

    update based on change made in class Menu to Console

--- src/OfficeService.java
+++ src/OfficeService.java
@@ -3,8 +3,11 @@ import java.text.ParseException;
 public class OfficeService {
     public static void main(String[] args) throws ParseException {
 
-        Menu menu = new Menu();
-        menu.startMenu();
-        
+
+        Console console = new Console();
+        console.startMenu();
+        console.endMenu();
+
+
     }
 }
