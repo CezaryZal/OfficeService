b2882bafb9c14d65022bf3b2babcff74d1ccb52a
Cezary Zalewski <czarek0821@gmail.com>
2019-05-30 12:01:29 +0200

    upgrade class
    add contribute to back last windows
    create new function to display specific info and update database

--- src/Console.java
+++ src/Console.java
@@ -12,60 +12,101 @@ public class Console extends Commands {
             displayMainMenu();
             anser = scanner.nextLine();
 
-            switch (anser) {
-                case "1": {
-                    displayBasicInfoAboutAllEmployments();
+            if (anser.equalsIgnoreCase("stop program")){
+                continue;
+            }
+            switch (Integer.valueOf(anser)) {
+                case 1: {
+                    displayBasicInfoAboutAllEmployees();
                     break;
                 }
-                case "2": {
+                case 2: {
                     do {
-                        displayOptionForSelectDetailsData();
+                        displayOptionToSelectDetailsData();
+                        displayHeaderToBackLastWindow();
                         anser = scanner.nextLine();
 
-                        switch (anser) {
-                            case "1": {
-                                displayOptionForSelectCategoryData();
+                        switch (Integer.valueOf(anser)) {
+                            case 1: {
+                                displayOptionToSelectCategoryData();
                                 //W przyszłości zostaną dopisane wszystkie możliwe konfiguracje wyświetlania danych
                                 anser = scanner.nextLine();
-                                displayAnswerEmploymentsShortDataAndChooseType(anser);
+                                displayAnswerEmployeesShortDataAndChooseType(anser);
                                 break;
                             }
-                            case "2": {
+                            case 2: {
                                 displayListEmploymentsShortData();
                                 anser = scanner.nextLine();
-                                displayBasicInfoAboutEmployerById(anser);
+                                displayBasicInfoAboutEmployeeById(anser);
                                 break;
+                            }
+                            case 3: {
+                                displayOptionToSelectCategoryToDisplaySpecificInfo();
+                                displayHeaderToBackLastWindow();
+                                anser = scanner.nextLine();
+                                if (anser.equalsIgnoreCase("back")) {
+                                    break;
+                                }
+                                setCategoryToDisplaySpecificInfo(anser);
+                                displayHeaderToSelectSpecificInfoFromEBI();
+                                displayHeaderToBackLastWindow();
+                                anser = scanner.nextLine();
+                                if (anser.equalsIgnoreCase("back")) {
+                                    break;
+                                }
+                                displayAllEmployeesByAttribute(anser);
+                                break;
+                                //można zrobić wyszukiwanie po samym roku zatrudnienia
                             }
                         }
                     } while (!anser.equalsIgnoreCase("back"));
                     break;
                 }
-                case "3": {
-                    displayHeaderToCreateNewEmployer();
+                case 3: {
+                    displayHeaderToCreateNewEmployee();
+                    displayHeaderToBackLastWindow();
                     anser = scanner.nextLine();
                     if (anser.equalsIgnoreCase("back")) {
                         break;
                     }
-                    sentAnswersToCreateNewEmployer(anser);
+                    setAnswersToCreateNewEmployee(anser);
                     break;
                 }
-                case "4": {
-                    displayHeaderToDeleteEmployer();
+                case 4: {
+                    displayHeaderToDeleteEmployee();
+                    displayHeaderToBackLastWindow();
                     anser = scanner.nextLine();
                     if (anser.equalsIgnoreCase("back")) {
                         break;
                     }
-                    sentCommandToDeleteEmployer(anser);
+                    setCommandToDeleteEmployee(anser);
+                    break;
+                }
+                case 5: {
+                    displayHeaderToSelectEmployeeToUpdateEBI();
+                    displayHeaderToBackLastWindow();
+                    anser = scanner.nextLine();
+                    if (anser.equalsIgnoreCase("back")) {
+                        break;
+                    }
+                    setIdToUpdateEmployeeFromEBI(anser);
+                    displayOprionToSelectCategoryToUpdateEBI();
+                    displayHeaderToBackLastWindow();
+                    if (anser.equalsIgnoreCase("back")) {
+                        break;
+                    }
+                    anser = scanner.nextLine();
+                    setCategoryToUpdateEBI(anser);
+                    displayOptionToUpdateEBIFromCategory();
+                    anser = scanner.nextLine();
+                    setNewDataAndUpdateEBI(anser);
+                    displayHeaderAfterUpdateEmployee();
                     break;
                 }
 
-
-                //Przy opcjach dodatkowych można umieścić filtrację po np. numerze departamentu (dziedziczenie nadklas)
             }
 
         } while (!anser.equalsIgnoreCase("stop Program"));
-        {
-        }
     }
 
     public void endMenu() {
